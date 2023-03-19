d71939a9006786cb78882e089985d420766511fa
viesuki <qszheng13@163.com>
2023-03-19 12:24:35 +0800

    ...

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,8 @@ import { SITE } from "./src/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: SITE.website,
+  site: "https://viesuki.github.io",
+  base: "/ablog",
   integrations: [
     tailwind({
       config: {
